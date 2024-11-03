95521a1131f39e74a6cca762655d610e559dee61
alire7a <alire7a.a@gmail.com>
2024-11-03 19:40:24 +0330

    first commit

---
+++ src/components/Landing/Hero/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import s from "./style.module.scss"
+const Hero = () => {
+    return (
+        <div className={s.container}>
+            blah blah
+        </div>
+    );
+};
+
+export default Hero;
