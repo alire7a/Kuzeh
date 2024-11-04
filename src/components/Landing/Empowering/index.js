92eff3193549358c5782733810dc91a756d9c9ec
iHolaa <erfan.fun.af@gmail.com>
2024-11-04 20:23:00 +0330

    Empowering Section design Finished but Still need Some Update For content on the images !

--- src/components/Landing/Empowering/index.js
+++ src/components/Landing/Empowering/index.js
@@ -2,8 +2,25 @@ import React from 'react';
 
 const Empowering = () => {
     return (
-        <div>
-
+        <div className={"padLayout !pt-[100px] !pb-[120px] flex flex-col gap-20"}>
+            <div className={"flex gap-20"}>
+                <h2 className={"font-semibold text-[44px] leading-[52px] w-[660px]"}>
+                    Empowering businesses through global trade
+                </h2>
+                <div className={"text-20 leading-[26px] w-[660px]"}>
+                    Kouzeh offers one-stop B2B trading solutions for global small and medium-sized businesses,
+                    empowering them to transform through digital trade, grasp opportunities, and accelerate growth
+                    internationally.
+                </div>
+            </div>
+            <div className={"flex items-center gap-5"}>
+                <div>
+                    <img src="/images/Empowering/pic1.png" alt=""/>
+                </div>
+                <div>
+                    <img src="/images/Empowering/pic2.png" alt=""/>
+                </div>
+            </div>
         </div>
     );
 };
